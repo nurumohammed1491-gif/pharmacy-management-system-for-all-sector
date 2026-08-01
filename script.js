33ce0ea9db5afe4f64cad78fcad81c21467fc6f0
nurumohammed1491-gif <nurumohammed1491@gmail.com>
2026-08-01 14:42:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -128,4 +128,21 @@ if (dispensingForm) {
 
     dispensingForm.reset();
   });
+}let loginForm = document.getElementById("loginForm");
+
+if (loginForm) {
+  loginForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if (username === "admin" && password === "1234") {
+      alert("Login successful!");
+
+      window.location.href = "index.html";
+    } else {
+      alert("Wrong username or password!");
+    }
+  });
 }
